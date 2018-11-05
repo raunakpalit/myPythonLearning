432815b46f6253842a002330801f1ada17fc6d7f
Palit Raunak <gotemahesh@johndeere.com>
2018-11-05 17:57:24 +0530

    Added new files

--- venv/tuples.py
+++ venv/tuples.py
@@ -7,25 +7,41 @@
 welcome = "Welcome to my nightmare", "Alice Cooper", 1975
 bad = "Bad Company", "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+# imelda = "More Mayhem", "Emilda May", 2011
+# metallica = "Ride the Lightning", "Metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+imelda = "More Mayhem", "Emilda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
 
-imelda = imelda[0], "Imelda May", imelda[2]
 print(imelda)
-
-title, artist, year = imelda
+title, artist, year, tracks = imelda
 print(title)
 print(artist)
 print(year)
+print(tracks)
 
 
-metallica2 = ["Ride the Lightning", "Metallica", 1984]
-print(metallica2)
-metallica2[0] = "Master of Puppets"
-print(metallica2)
- 
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+
+# imelda = imelda[0], "Imelda May", imelda[2]
+# print(imelda)
+
+# title, artist, year = imelda
+# print(title)
+# print(artist)
+# print(year)
+
+
+# metallica2 = ["Ride the Lightning", "Metallica", 1984]
+# print(metallica2)
+# metallica2[0] = "Master of Puppets"
+# print(metallica2)
+
+# title, artist, year = imelda
+# print(title)
+# print(artist)
+# print(year)
+
