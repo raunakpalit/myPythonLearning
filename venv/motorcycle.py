70518ca26ab84b19f2716a6a81e68aaca205070d
Palit Raunak <gotemahesh@johndeere.com>
2018-11-21 16:34:32 +0530

    Added files for shelve and pickling

--- venv/motorcycle.py
+++ venv/motorcycle.py
@@ -1,16 +1,20 @@
 import shelve
 
-with shelve.open("bike2") as bike:
-    # bike["make"] = "Honda"
-    # bike["model"] = "250 dream"
-    # bike["colour"] = "red"
-    # bike["engine_size"] = 250
-    for key in bike:
-        print(key)
+with shelve.open("bike") as bike:
+    import pdb
+    pdb.set_trace()
+    '''
+    bike["make"] = "Honda"
+    bike["model"] = "250 dream"
+    bike["colour"] = "red"
+    bike["engin_size"] = 250
+    # for key in bike:
+    #     print(key)
     
-    print('=' * 40)
+    # print('=' * 40)
 
     print(bike["engine_size"])
-    print(bike["engin_size"])
+    # print(bike["engin_size"])
     print(bike["colour"])
+    '''
 
--- venv/shelveexample.py
+++ venv/shelveexample.py
@@ -1,12 +1,33 @@
 import shelve
 
 with shelve.open('ShelfTest') as fruit:
-    fruit['orange'] = "a sweet, orange, citrus fruit"
-    fruit['apple'] = "good for making cider"
-    fruit['lemon'] = "a sour, yellow citrus fruit"
-    fruit['grape'] = "a small, sweet fruit growing in bunches"
-    fruit['lime'] = "a sour, green citrus fruit"
+    # fruit['orange'] = "a sweet, orange, citrus fruit"
+    # fruit['apple'] = "good for making cider"
+    # fruit['lemon'] = "a sour, yellow citrus fruit"
+    # fruit['grape'] = "a small, sweet fruit growing in bunches"
+    # fruit['lime'] = "a sour, green citrus fruit"
 
-    print(fruit["lemon"])
-    print(fruit["grape"])
-print(fruit)
+    # print(fruit["lemon"])
+    # print(fruit["grape"])
+    # fruit['lime'] = "great with tequila"
+    # for snack in fruit:
+    #     print(snack + ": " + fruit[snack])
+# print(fruit)
+
+    # while True:
+    #     shelve_key = input("Please enter a fruit: ")
+    #     if shelve_key == "quit":
+    #         break
+    #     description = fruit.get(shelve_key, "We don't have a " + shelve_key)
+    #     print(description)
+
+    # for f in fruit:
+    #     print(f + " - " + fruit[f])
+
+    for v in fruit.values():
+        print(v)
+    print(fruit.values())
+
+    for i in fruit.items():
+        print(i)
+    print(fruit.items())
