fe3dc17f692827ee0b99c7d52a7cfff5114f854f
Palit Raunak <gotemahesh@johndeere.com>
2019-03-13 17:25:11 +0530

    Added

--- venv/modulesAndFunctions/ScopeAndNamespaces/filesAndDirectories.py
+++ venv/modulesAndFunctions/ScopeAndNamespaces/filesAndDirectories.py
@@ -8,7 +8,7 @@ def list_directories(s):
         for f in files:
             current_dir = os.path.join(d, f)
             if os.path.isdir(current_dir):
-                print("\t" * tab_stop + "Directory " +f)
+                print("\t" * tab_stop + "Directory " + f)
                 tab_stop += 1
                 dir_list(current_dir)
                 tab_stop -= 1
@@ -23,9 +23,6 @@ def list_directories(s):
         print(s + " does not exist")
 
 
-
-
-
 # listing = os.walk('.')
 # for root, directories, files in listing:
 #     print(root)
@@ -35,3 +32,4 @@ def list_directories(s):
 #         print(file)
 
 list_directories('.')
+
