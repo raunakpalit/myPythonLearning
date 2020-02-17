edd6152e7f47a4ba99b35a4ae6ea62b254be9d55
Raunak Palit <palitraunak@johndeere.com>
2020-02-17 21:00:48 +0530

    Added code for doubly linked list features

--- DataStructures/doubly_linked_list.py
+++ DataStructures/doubly_linked_list.py
@@ -42,10 +42,113 @@ class DoublyLinkedList:
             print(cur.data)
             cur = cur.next
 
+    def add_after_node(self, key, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                next_node = cur.next
+                next_node.prev = new_node
+                new_node.next = next_node
+                cur.next = new_node
+                new_node.prev = cur
+            cur = cur.next
+
+    def add_before_node(self, key, data):
+        new_node = Node(data)
+        prev_node = None
+        cur = self.head
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                prev_node = cur.prev
+                prev_node.next = new_node
+                new_node.next = cur
+                cur.prev = new_node
+                new_node.prev = prev_node
+            cur = cur.next
+
+    def delete(self, key):
+        cur = self.head
+        prev = None
+        while cur:
+            if cur.prev is None and cur.data == key:
+                nxt = cur.next
+                self.head = nxt
+                nxt.prev = None
+                return
+            elif cur.next is None and cur.data == key:
+                prev = cur.prev
+                prev.next = None
+                return
+            elif cur.data == key:
+                prev = cur.prev
+                nxt = cur.next
+                prev.next = nxt
+                nxt.prev = prev
+            cur = cur.next
+
+    def reverse(self):
+        prev = None
+        cur = self.head
+        while cur:
+            prev = cur.prev
+            nxt = cur.next
+            cur.next = prev
+            cur.prev = nxt
+            cur = nxt
+        if prev:
+            self.head = prev.prev
+
+    def remove_duplicates(self):
+        temp = []
+        cur = self.head
+        while cur:
+            if cur.data not in temp:
+                temp.append(cur.data)
+            else:
+                if cur.next:
+                    prev = cur.prev
+                    nxt = cur.next
+                    nxt.prev = prev
+                    prev.next = nxt
+                else:
+                    if cur.data in temp:
+                        prev = cur.prev
+                        prev.next = None
+            
+            cur = cur.next
+            
+    def pairs_with_sum(self, sum_val):
+        pass
+
 dllist = DoublyLinkedList()
 dllist.append(1)
 dllist.append(2)
 dllist.append(3)
 dllist.append(4)
-dllist.prepend(0)
+dllist.append(5)
+
+dllist.pairs_with_sum(5)
+# dllist.reverse()
+# dllist.add_before_node(4, 5)
+# dllist.delete(4)
+# dllist.prepend(0)
+# dllist.append(8)
+# dllist.append(4)
+# dllist.append(4)
+# dllist.append(6)
+# dllist.append(4)
+# dllist.append(8)
+# dllist.append(4)
+# dllist.append(10)
+# dllist.append(12)
+# dllist.append(12)
+
+# dllist.remove_duplicates()
 dllist.print_list()
