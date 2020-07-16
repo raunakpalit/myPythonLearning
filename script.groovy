8d25aafca653b9ca268527658a2cc9455e9e54df
Raunak <palit.raunak@gmail.com>
2020-07-17 02:40:28 +0530

    Added script

--- script.groovy
+++ script.groovy
@@ -2,12 +2,8 @@ def testApp() {
     echo "Testing the application..."
     directory1 = pwd()
     echo "Direct is ${directory1}"
-    dh = new File('.')
-    dh.eachFile {
-        println(it)
-    }
 
-    File myFile = new File("test.py")
+    File myFile = new File("test")
     println myFile.text
 }
 
