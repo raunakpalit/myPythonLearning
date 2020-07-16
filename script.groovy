67f39777b7724cee3f84d49dcc667b2a4e1871ea
Raunak <palit.raunak@gmail.com>
2020-07-17 02:27:44 +0530

    Added script

--- script.groovy
+++ script.groovy
@@ -2,6 +2,8 @@ def testApp() {
     echo "Testing the application..."
     directory1 = pwd()
     echo "Direct is ${directory1}"
+    content = dir
+    echo "content is ${content}"
 
     File myFile = new File("test.py")
     println myFile.text
