d5336deb980d6b6087832f581293c3d110e964d7
Raunak <palit.raunak@gmail.com>
2020-07-19 04:08:58 +0530

    Added script

--- fetchTest.py
+++ fetchTest.py
@@ -3,4 +3,5 @@ import sys
 print("Hello World! My name is Raunak Palit")
 
 dataProvided = str(sys.argv[1])
-print("Data provided is: {}".format(dataProvided))
+for line in dataProvided:
+    print(line)
